638a7e130c83f5100f3be5a17dda9ca93817436d
mohamad taghipour <tpmamad@gmail.com>
2019-02-05 18:33:16 +0330

    Welcome Page Added

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,17 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-   .sass('resources/sass/app.scss', 'public/css');
+mix.babel([
+    'resources/js/app.js',
+    'resources/js/main.js',
+    'resources/js/bootstrap.min.js',
+    'resources/js/jquery-3.2.1.slim.min.js',
+    'resources/js/popper.min.js'
+], 'public/js/app.js');
+
+
+mix.styles([
+    'resources/css/register.css',
+    'resources/css/main.css',
+    'resources/css/bootstrap.min.css'
+], 'public/css/app.css');
